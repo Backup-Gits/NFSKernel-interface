65913489ae8cb57fa8023572a441ab76c5fb0e43
AnGgIt86 <jarbull86@gmail.com>
2021-07-11 12:29:24 +0700

    add build for tulip

---
+++ tulip/oldcam/rm.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2021 a xyzprjkt property
+#
+
+export USE_CCACHE=1
+export CCACHE_DIR="$HOME/.ccache"
+export LOCALVERSION=-OldCam
+export TZ=Asia/Jakarta
+export KERNEL_NAME=NFS-Kernel-4.4-OldCam
+export KERNEL_SOURCE=https://github.com/AnGgIt86/kernel-sdm660.git
+export KERNEL_BRANCH=deagle
+export DEVICE_CODENAME=tulip
+export DEVICE_DEFCONFIG=deagle_tulip_defconfig
+export ANYKERNEL=https://github.com/AnGgIt86/AnyKernel3
+export TG_TOKEN=1852697615:AAGKDF9cYNnTY4Ylm7XjBrsssS31eTtqYfk
+export TG_CHAT_ID=-1001580307414
+export BUILD_USER=xiaomi
+export BUILD_HOST=nfs-projects
+git config --global user.email "jarbull86@gmail.com"
+git config --global user.name "AnGgIt86"
+# Main Declaration
+KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=$DEVICE_DEFCONFIG # IMPORTANT ! Declare your kernel source defconfig file here.
+export KBUILD_BUILD_USER=$BUILD_USER # Change with your own name or else.
+export KBUILD_BUILD_HOST=$BUILD_HOST # Change with your own hostname.
+
+cd ${KERNEL_ROOTDIR}
+make O=out clean && make O=out mrproper
+rm -rf AnyKernel
