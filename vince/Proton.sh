5a39a0d7175c2b0fefdf38e0c466dfe81a6c9565
AnGgIt86 <jarbull86@gmail.com>
2021-07-17 00:26:51 -0700

    vince update

--- vince/Proton.sh
+++ vince/Proton.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
-#
-# Copyright (C) 2021 a xyzprjkt property
-#
 
-export USE_CCACHE=1
-export CCACHE_DIR="$HOME/.ccache"
-export LOCALVERSION=
-export TZ=Asia/Jakarta
-export KERNEL_NAME=NFS-Kernel-4.9
-export KERNEL_SOURCE=https://github.com/AnGgIt86/kernel_dark_ages_vince.git
-export KERNEL_BRANCH=eleven
-export DEVICE_CODENAME=vince
-export DEVICE_DEFCONFIG=vince_defconfig
-export ANYKERNEL=https://github.com/AnGgIt86/AnyKernel3
-export TG_TOKEN=1852697615:AAGKDF9cYNnTY4Ylm7XjBrsssS31eTtqYfk
-export TG_CHAT_ID=-1001580307414
-export BUILD_USER=xiaomi
-export BUILD_HOST=nfs-projects
-git config --global user.email "jarbull86@gmail.com"
-git config --global user.name "AnGgIt86"
+export CONFIG_COMPAT_VDSO=y
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
@@ -74,7 +56,7 @@ tg_post_msg "<b>NFSKernel-Redmi 5 Plus (vince)</b>%0ABuilder Name : <code>${KBUI
 
 # Compile
 compile(){
-tg_post_msg "<b>NFSKernel-Redmi 5 Plus (vince):</b><code>Membangun Kernel sedang di muali, Sabar ya sayank. Gak lama kok..</code>"
+tg_post_msg "<b>NFSKernel-Redmi 5 Plus (vince):</b><code>Membangun Kernel sedang di mulai, Sabar ya bro. Gak lama kok..</code>"
 cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
