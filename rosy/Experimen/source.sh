fb33d0a39ea1ac69cb6461182079fc9daf5f2a08
AnGgIt86 <jarbull86@gmail.com>
2021-07-25 12:56:00 +0000

    test toolchain new

--- rosy/Experimen/source.sh
+++ rosy/Experimen/source.sh
@@ -7,7 +7,7 @@ export KERNEL_NAME=NFS-Kernel-Experiment
 export KERNEL_SOURCE=https://github.com/AnGgIt86/kernel_xiaomi_rosy-4.9.git
 export KERNEL_BRANCH=eleven
 export DEVICE_CODENAME=rosy
-export DEVICE_DEFCONFIG=rosy_defconfig
+export DEVICE_DEFCONFIG=rosy-perf_defconfig
 export ANYKERNEL=https://github.com/AnGgIt86/AnyKernel3
 export TG_TOKEN=1852697615:AAGKDF9cYNnTY4Ylm7XjBrsssS31eTtqYfk
 export TG_CHAT_ID=-1001580307414
@@ -28,7 +28,7 @@ tg_post_msg() {
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 $KERNEL_SOURCE -b $KERNEL_BRANCH $DEVICE_CODENAME
-git clone --depth=1 https://github.com/AnGgIt86/NFS-TC.git NFS-Toolchain
+git clone --depth=1 https://github.com/AnGgIt86/NeedForSpeed-Clang.git NFS-Toolchain
 
 curl https://raw.githubusercontent.com/AnGgIt86/NFSKernel-interface/main/rosy/Experimen/NFS.sh -o NFS.sh
 
@@ -36,7 +36,6 @@ curl -s -X POST "$BOT_MSG_URL2/sendSticker" \
 -d sticker="CAACAgUAAx0CXjGT1gACAeRg69dV3PYH_z8EZQnV9D9MubhVCwAClAAD7OCaHulbTgv4Q5nsIAQ" \
 -d chat_id="$TG_CHAT_ID"
         
-tg_post_msg "<b>NFSKernel-Experiment-(rosy):</b><code>Started build witch GCC</code>"
 tg_post_msg "<b>NFSKernel-Experiment-(rosy):</b><code>Started build witch NFS Clang</code>"
 echo "started build witch NFS Clang"
 bash NFS.sh
