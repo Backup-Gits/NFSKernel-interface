f96406a84a0745463145a37990169bcfb785cc9b
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-07-28 23:14:27 +0700

    Update source.sh

--- rosy/finix/source.sh
+++ rosy/finix/source.sh
@@ -30,8 +30,11 @@ echo "Downloading few Dependecies . . ."
 git clone --depth=1 $KERNEL_SOURCE -b $KERNEL_BRANCH $DEVICE_CODENAME
 git clone --depth=1 https://github.com/AnGgIt86/arm64-gcc NFS-Toolchain
 git clone --depth=1 https://github.com/AnGgIt86/arm64-gcc NFS-Toolchain2
+git clone --depth=1 https://github.com/AnGgIt86/NeedForSpeed-Clang NFS-Clang
 
 wget https://raw.githubusercontent.com/AnGgIt86/NFSKernel-interface/main/rosy/finix/NFS.sh
+wget https://raw.githubusercontent.com/AnGgIt86/NFSKernel-interface/main/rosy/finix/NFS-clang.sh
+wget https://raw.githubusercontent.com/AnGgIt86/NFSKernel-interface/main/rosy/finix/rm.sh
 
 curl -s -X POST "$BOT_MSG_URL2/sendSticker" \
 -d sticker="CAACAgUAAx0CXjGT1gACAeRg69dV3PYH_z8EZQnV9D9MubhVCwAClAAD7OCaHulbTgv4Q5nsIAQ" \
@@ -39,4 +42,15 @@ curl -s -X POST "$BOT_MSG_URL2/sendSticker" \
         
 tg_post_msg "<b>Finix-kernel-(rosy):</b><code>Started build witch NFS GCC</code>"
 echo "started build witch NFS GCC"
+chmod +x NFS.sh
 bash NFS.sh
+echo "membersihkan config sebelumnya"
+bash rm.sh
+tg_post_msg "<b>NFSKernel-normal-(rosy):</b><code>Started build witch NeedForSpeed Clang</code>"
+echo "Started build witch NeedForSpeed Clang"
+chmod +x NFS-clang.sh
+bash NFS-clang.sh
+tg_post_msg "<b>NFSKernel-normal-(rosy):</b><code>Building completed...</code>"
+curl -s -X POST "$BOT_MSG_URL2/sendSticker" \
+-d sticker="CAACAgIAAx0CXjGT1gACAeVg69gXIw-a6h1nvmmaub51tQQwCgACLQMAAsbMYwIquW4nbs0crSAE" \
+-d chat_id="$TG_CHAT_ID"
